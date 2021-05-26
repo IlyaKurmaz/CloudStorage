9897d1b5535eea5bdf6d548f6b4a81fddafc3ee1
Illia Kurmaz <ilya.kurmaz@gmail.com>
2021-05-26 15:54:26 +0300

    feat: project reorganization and proper naming

---
+++ CloudStorage.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CloudStorage.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ CloudStorage.Persistance/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CloudStorage.Persistance
+{
+    public class Class1
+    {
+    }
+}
